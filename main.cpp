8e3b85b125871c82a8cde8df6da88b11b317ba8e
Eduardo Rampon Meireles <eduardomeireles3232@gmail.com>
2020-07-16 15:37:56 -0300

    keys, bucket and node

--- main.cpp
+++ main.cpp
@@ -2,9 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+typedef struct sHashNode {
+  struct sHashNode *next;
+  struct sHashNode *prev;
 
-  printf("Hello World");
+  int name;
+} HashNode;
+
+typedef struct sHashBucket {
+  struct sHashNode *head;
+  struct sHashNode *tail;
+  int index;
+  int bucketSize;
+} Hashbucket;
+
+typedef struct sKeys {
+  struct sHashBucket *head;
+  struct sHashBucket *tail; 
+  int size;
+} Keys;
+
+int main() {
+  Keys* keys = (Keys*)malloc(sizeof(Keys));
+  keys->size = 41;
+
+  printf("%i", keys->size );
 
 
   return 1;
